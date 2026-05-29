424ecd9b783e7bd9202a39273f69e55a1e3601cf
kamin_ruslan <kaminruslan@yandex.ru>
2026-05-30 00:50:34 +0400

    Add new param test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,7 +5,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import helpers.Attach;
+import halpers.Attach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -20,9 +20,11 @@ public class TestBase
     }
     @BeforeAll
     static  void  beforeAll() {
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x2080";
-//      Configuration.browserVersion= "144.0";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
